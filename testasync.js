af785663dc0299e9e4216b933c91e2e1e355e30a
yuxiu <yuxiu@showjoy.com>
2016-06-22 16:57:14 +0800

    delete: 删除一些无用测试文件

--- testasync.js
+++
@@ -1,9 +0,0 @@
-var fs = require('fs');
-var path = require('path');
-async function abc(...arg){
-  await process.nextTick(function(){console.log(333)},arg);
-  var content = await fs.readFile('package.json','utf8');
-  console.log(content);
-}
-
-abc(500)

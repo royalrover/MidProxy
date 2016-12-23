3f5d158bd2724f6e99b85c5785a7e49de32378da
yuxiu <yuxiu@showjoy.com>
2016-12-23 11:57:26 +0800

    fix(回滚登录判断，仍使用原先判断tgc的方式。判断userInfo会出现认证死循环):

--- midwares/auth/index.js
+++ midwares/auth/index.js
@@ -2,11 +2,12 @@
 
 // 基于尚妆（达人店）认证体系，通过cookie的字段判断是否登录
 module.exports.check = function*(next){
-  var cookie,userInfo;
+  var cookie,tgc,rememberNum;
   cookie = this.cookies;
-  // 该字段标识是否登录
-  userInfo = cookie.get('userInfo');
-  if(!userInfo){
+  // 该字段为umbrella认证系统写入的ticket
+  tgc = cookie.get('tgc');
+  rememberNum = cookie.get('um_remember');
+  if(!tgc && !rememberNum){
     this.login = false;
   }else{
     this.login = true;
